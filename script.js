0a975502fef7fabb425817f93a23df3f6ff5fc7c
sushil026 <68173862+sushil026@users.noreply.github.com>
2023-07-05 20:46:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+const password = document.getElementById("pswd");
 const sliderEl = document.getElementById("ranger");
 const output = document.getElementById("rangeVal");
+const upper = document.getElementById("Uppercase");
+const lower = document.getElementById("Lowercase");
+const nums = document.getElementById("Numbers");
+const symbols = document.getElementById("Symbols");
 function display(){
     output.innerHTML = sliderEl.value;
 }
@@ -13,50 +18,69 @@ function copy() {
     alert("Copied the text: " + copyText.innerHTML);
 }
 function checkUc(){
-    if(parseInt(output.innerHTML) < 10){
-        if( document.getElementById("Uppercase").checked){
-            output.innerHTML = parseInt(output.innerHTML) + 1; 
+        if( upper.checked && parseInt(output.innerHTML) < 4){
+            sliderEl.value = parseInt(output.innerHTML) + 1;
+            output.innerHTML = parseInt(output.innerHTML) + 1;
         }
-        else{
-            output.innerHTML = parseInt(output.innerHTML) - 1;
-        }
-        sliderEl.value = parseInt(output.innerHTML);
         ranger();
-    }
 }
 function checkLc(){
-    if(parseInt(output.innerHTML) < 10){
-        if( document.getElementById("Lowercase").checked){
+        if( lower.checked && parseInt(output.innerHTML) < 4){
+            sliderEl.value = parseInt(output.innerHTML) + 1;
             output.innerHTML = parseInt(output.innerHTML) + 1;
         }
-        else{
-            output.innerHTML = parseInt(output.innerHTML) - 1;
-        }
-        sliderEl.value = parseInt(output.innerHTML);
         ranger();
-    }
 }
 function checkNum(){
-    if(parseInt(output.innerHTML) < 10){
-        if( document.getElementById("Numbers").checked){
+        if( nums.checked && parseInt(output.innerHTML) < 4){
+            sliderEl.value = parseInt(output.innerHTML) + 1;
             output.innerHTML = parseInt(output.innerHTML) + 1;
         }
-        else{
-            output.innerHTML = parseInt(output.innerHTML) - 1;
-        }
-        sliderEl.value = parseInt(output.innerHTML);
         ranger();
-    }
 }
 function checkSym(){
-    if(parseInt(output.innerHTML) < 10){
-        if( document.getElementById("Symbols").checked){
+        if( symbols.checked && parseInt(output.innerHTML) < 4){
+            sliderEl.value = parseInt(output.innerHTML) + 1;
             output.innerHTML = parseInt(output.innerHTML) + 1;
         }
-        else{
-            output.innerHTML = parseInt(output.innerHTML) - 1;
+    ranger();
+}
+function generate() {
+    var res = "";
+    var req = output.innerHTML;
+    var caps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var smalls = 'abcdefghijklmnopqrstuvwxyz';
+    var numbs = '0123456789';
+    var sym = '!@#$%^&_-';
+    if( upper.checked || lower.checked || nums.checked || symbols.checked){
+        var all = '';
+        all += upper.checked ? caps: "";
+        all += lower.checked ? smalls: "";
+        all += nums.checked ? numbs: "";
+        all += symbols.checked ? sym: "";
+        while (res.length < req) {
+            res += (all.charAt(Math.floor(Math.random() * all.length)));
         }
-        sliderEl.value = parseInt(output.innerHTML);
-        ranger();
+        password.innerHTML = res;
+    }
+    else{    
+        alert("Please select required Characters for the Password");
+        console.log("Please select required Characters for the Password");  
+    }
+}
+function strong(){
+    const lev = document.getElementsByClassName("levels");
+    const checks = document.querySelectorAll('input[type="checkbox"]');
+    for (let i = 0; i < lev.length; i++) {
+        lev[i].style.backgroundColor = "#18171F";
+    }
+    var str = 0;
+    for (let i = 0; i < checks.length; i++) {   
+        if( checks[i].checked ){
+            str++;
+        }
+    }  
+    for (let i = 0; i < str; i++) {
+        lev[i].style.backgroundColor = "#A4FFAF";
     }
 }
